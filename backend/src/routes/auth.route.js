56edd67b3674d8d144009a77f786bfb2c70a8e09
Sayan Duary <sayanduary@gmail.com>
2026-07-06 11:42:30 +0530

    update

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { auth } from "../middleware/authentication.js";
 import {
   login,
   logout,
   signup,
   updateProfile,
 } from "../controller/authController.js";
+import { auth } from "../middleware/authentication.js";
 
 const router = express.Router();
 
@@ -13,5 +13,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/update-profile", auth, updateProfile);
+router.get("/check", auth, (req, res) => {
+  res.status(200).json(req.user);
+
+});
 
 export default router;
